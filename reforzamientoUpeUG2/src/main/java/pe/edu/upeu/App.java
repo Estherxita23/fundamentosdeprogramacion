02fa4da011d1f05975ded27d3a14c28077f346d8
Estherxita23 <raquel.machaca@upeu.edu.pe>
2021-10-24 21:11:22 +0000

    g2 mmmlll

---
+++ reforzamientoUpeUG2/src/main/java/pe/edu/upeu/App.java
@@ -0,0 +1,36 @@
+package pe.edu.upeu;
+
+import java.util.Date;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    static void tiposdatos(){
+        byte a=1;
+        short b= 8;
+        int c=87;
+        float d=84.5f;
+       double e= 87.21;
+        boolean f=true;
+        
+        char h='L';
+        String g ="+";
+        Integer xx;
+        Double xxx;
+        Date ss;
+    } 
+       
+    public static void operadores () {
+        
+        int a=7;
+        int b=2;
+        // operadores aritmeticos 
+    }
+    public static void controlstructura () {
+        
+    }
+    public static void main( String[] args ){
+        System.out.println( "Hello World!" ); }}
